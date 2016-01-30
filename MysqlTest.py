a26be932077e9d5ed5b3a4ea9df1c33ac1a19775
Ella Xue <ellaxue007@gmail.com>
2016-01-30 02:29:48 -0500

    revise importing file into database

--- MysqlTest.py
+++ MysqlTest.py
@@ -1,21 +1,21 @@
 import MySQLdb
 
-db = MySQLdb.connect("localhost","root","wb3331314","TESTDB")
+db = MySQLdb.connect("localhost","root","wb3331314","TESTDB",local_infile = 1)
 
 cursor = db.cursor()
 cursor.execute("DROP TABLE IF EXISTS EMPLOYEE")
 sql = """CREATE TABLE EMPLOYEE(
-	FIRST_NAME CHAR(20) NOT NULL, LAST_NAME CHAR(20), AGE INT, SEX CHAR(1), 	INCOME FLOAT)"""
+	FIRST_NAME CHAR(20) NOT NULL, LAST_NAME CHAR(20), AGE INT, SEX CHAR(1), INCOME FLOAT)"""
 
 
 cursor.execute(sql)
 print 'created table EMPLOYEE'
 
  
-# Prepare SQL query to INSERT a record into the database.
-sql = """INSERT INTO EMPLOYEE(FIRST_NAME,
-         LAST_NAME, AGE, SEX, INCOME)
-         VALUES ('Mac', 'Mohan', 20, 'M', 2000)"""
+# Prepare SQL query to IMPORT data file into the database.
+sql = """LOAD DATA LOCAL INFILE 'input.txt' INTO TABLE EMPLOYEE
+		FIELDS TERMINATED BY '\t'
+		LINES TERMINATED BY '\n' """
 try:
    # Execute the SQL command
    cursor.execute(sql)
@@ -27,8 +27,9 @@ except:
    	db.rollback()  
 
 cursor.execute("""SELECT * FROM EMPLOYEE""")
-for row in cursor:
-	print (row[1])
+result = cursor.fetchall()
+for row in result:
+	print row
 
 db.close()
 
